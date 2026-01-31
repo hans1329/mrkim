abc42f261c590e53e9c04fece8908cc440a73c16
hans1329 <hans1329@naver.com>
2026-01-31 14:04:18 +0000

    Changes

--- src/components/chat/FloatingServiceChatButton.tsx
+++ src/components/chat/FloatingServiceChatButton.tsx
@@ -13,7 +13,7 @@ export function FloatingServiceChatButton() {
       className="fixed bottom-6 right-6 h-16 w-16 rounded-full shadow-lg hover:scale-105 transition-transform z-40 bg-white hover:bg-white/90 p-1 animate-bounce-subtle"
       size="icon"
     >
-      <img src={chatbotIcon} alt="김비서" className="h-12 w-12 object-contain" style={{ filter: 'invert(37%) sepia(93%) saturate(1352%) hue-rotate(200deg) brightness(97%) contrast(101%)' }} />
+      <img src={chatbotIcon} alt="김비서" className="h-12 w-12 object-contain" />
     </Button>
   );
 }
