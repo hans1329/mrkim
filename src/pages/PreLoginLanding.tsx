a878d8529819af86b1ce8d8619aa627a53f2e161
hans1329 <hans1329@naver.com>
2026-01-31 13:37:19 +0000

    Changes

--- src/pages/PreLoginLanding.tsx
+++ src/pages/PreLoginLanding.tsx
@@ -6,7 +6,7 @@ import mainIllust from "@/assets/main-illust.webp";
 import mainIllust2 from "@/assets/main-illust2.webp";
 import qrCode from "@/assets/qr-code.png";
 import logo from "@/assets/icc-3.webp";
-import logoWhite from "@/assets/logo-white.webp";
+import logoWhite from "@/assets/icc-white.webp";
 const PreLoginLanding = () => {
   const navigate = useNavigate();
   const features = [{
