652c7f96af2b0929509cb90f76d951278c3b0959
hans1329 <hans1329@naver.com>
2026-03-09 11:08:19 +0000

    Preceding changes

--- src/pages/PitchDeck.tsx
+++ src/pages/PitchDeck.tsx
@@ -123,7 +123,7 @@ export default function PitchDeck() {
     >
       {/* Navigation Dots */}
       <nav className="fixed right-6 top-1/2 -translate-y-1/2 z-50 hidden md:flex flex-col gap-2">
-        {Array.from({ length: 12 }).map((_, i) => (
+        {Array.from({ length: 13 }).map((_, i) => (
           <button
             key={i}
             onClick={() => scrollToSection(i)}
