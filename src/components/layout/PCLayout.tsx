174eb56ff0eddcab3243dbb51c7bf4e08a1e809b
hans1329 <hans1329@naver.com>
2026-03-19 12:17:16 +0000

    Preceding changes

--- src/components/layout/PCLayout.tsx
+++ src/components/layout/PCLayout.tsx
@@ -12,7 +12,8 @@ import {
   HelpCircle,
   PanelLeftClose,
   PanelLeft,
-  User
+  User,
+  UserCheck
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
