d7872141dba5e3006f65b51dbf36d23d487713cb
hans1329 <hans1329@naver.com>
2026-04-01 17:20:31 +0000

    Preceding changes

--- src/components/tax-accountant/FilingTab.tsx
+++ src/components/tax-accountant/FilingTab.tsx
@@ -7,7 +7,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { supabase } from "@/integrations/supabase/client";
 import IrregularFilingSection from "./IrregularFilingSection";
 import { toast } from "sonner";
-import { useClassificationStats, useRunAIClassification } from "@/hooks/useTaxClassification";
+import { useClassificationStats, useAutoAIClassification } from "@/hooks/useTaxClassification";
 import {
   FileText,
   AlertCircle,
